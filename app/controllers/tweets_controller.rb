4bd42821c06e85c2e899c25602498266903be5bc
林佳志乃 <twinklestaryoshino@gmail.com>
2018-06-22 23:54:37 +0900

    大体の機能はできた

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,14 +1,17 @@
 class TweetsController < ApplicationController
 
+  before_action :move_to_index, except: :index
+
   def index
-    @tweets = Tweet.all
+    binding.pry
+    @tweets = Tweet.all.order("created_at DESC").page(params[:page]).per(5)
   end
 
   def new
   end
 
   def create
-    Tweet.create(tweet_params)
+    Tweet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
 
   private
@@ -16,6 +19,7 @@ class TweetsController < ApplicationController
     params.permit(:name, :image, :text)
   end
 
-
-
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 end
