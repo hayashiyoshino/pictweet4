fa9c3f27acc8ab79c98d0e52907181bb0b972af3
林佳志乃 <twinklestaryoshino@gmail.com>
2018-06-23 18:15:32 +0900

    user,tweet,commentモデルにアソシエーション定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,22 +1,40 @@
 class TweetsController < ApplicationController
 
-  before_action :move_to_index, except: :index
+  before_action :move_to_index, except: [:index, :show]
 
   def index
-    @tweets = Tweet.all.order("created_at DESC").page(params[:page]).per(5)
+    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5).order("created_at DESC")
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
   end
 
   def new
   end
 
   def create
-    Tweet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+      tweet.destroy if tweet.user_id == current_user.id
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params) if tweet.user.id == current_user.id
   end
 
 
   private
   def tweet_params
-    params.permit(:name, :image, :text)
+    params.permit(:image, :text)
   end
 
   def move_to_index
